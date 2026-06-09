e899126d069a624fc8240a4405fabad2b011d401
Tom Ventimiglia <tventimi@princeton.edu>
2026-06-09 12:23:12 -0400

    add query to WEB page title

--- webrenderer.js
+++ webrenderer.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const urlParams = new URLSearchParams(window.location.search)
+    var catalog = ""
+    var query = ""
     urlParams.forEach((value, key) => {
         if(key === "catalog") {
             document.getElementById("catalog").value = value
+            catalog = value
         } else if(key === "q") {
             document.getElementById("queryString").value = value
+            query = value
         }
     })
+    if(catalog && query) {
+        document.title = "LibLookup: " + catalog + " " + query
+    }
 })
