e3bc78d1a5234817aa74068b62cb51327cc0a4b6
Tom Ventimiglia <tventimi@princeton.edu>
2026-07-09 09:37:54 -0400

    repopulate query list from param

--- webrenderer.js
+++ webrenderer.js
@@ -9,15 +9,36 @@ urlParams.forEach((value, key) => {
         catalog = value
     } else if(key === "q") {
         document.getElementById("queryString").value = value
-        query = value
+        var searchTerms = document.getElementById("searchTerms")
+        var queryString = decodeURIComponent(value)
+        var queryTokens = tokenize(queryString)
+        for(var i = 0; i < queryTokens.length; i += 3) {
+            if(i == 0) {
+                searchTerms.add(new Option(queryTokens.slice(i,i+3).join(' ')))
+            } else {
+                searchTerms.add(new Option(queryTokens.slice(i,i+4).join(' ')))
+                i++
+            }
+        }
     } else if(key === "displayFields") {
         document.getElementById("displayFields").value = value
         displayFields = value
     } 
 })
+
 if(catalog && query) {
     document.title = "LibLookup: " + catalog + " " + query
-}       
+}     
+
+function tokenize(str) {
+    const regex = /"[^"]+"|[^\s]+/g;
+    let matches = [];
+    let match;
+    while ((match = regex.exec(str)) !== null) {
+        matches.push(match[0]);
+    }
+    return matches;
+}
 
 
 
