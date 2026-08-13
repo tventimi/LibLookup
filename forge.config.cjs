b34188eff0722fea2a7249da05f54f6c46257f90
Tom Ventimiglia <tventimi@princeton.edu>
2026-08-13 14:51:34 -0400

    remove redundant start plugin

--- forge.config.cjs
+++ forge.config.cjs
@@ -26,10 +26,6 @@ module.exports = {
     },
   ],
   plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
     // Fuses are used to enable/disable various Electron functionality
     // at package time, before code signing the application
     new FusesPlugin({
